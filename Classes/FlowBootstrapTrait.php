a7dd2be32fc7b4c105cbd6db4b7740e33bcf919d
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-06-18 19:56:45 +0200

    BUGFIX: improve Flow booting and mock current Request correctly

--- Classes/FlowBootstrapTrait.php
+++ Classes/FlowBootstrapTrait.php
@@ -8,8 +8,9 @@ use Neos\Flow\Configuration\ConfigurationManager;
 use Neos\Flow\Core\Booting\Exception\SubProcessException;
 use Neos\Flow\Core\Booting\Scripts;
 use Neos\Flow\Core\Bootstrap;
-use Neos\Flow\ObjectManagement\ObjectManagerInterface;
-use Neos\Flow\Tests\FunctionalTestRequestHandler;
+use Neos\Flow\Testing\RequestHandler\RuntimeSequenceHttpRequestHandler;
+use Neos\Http\Factories\ServerRequestFactory;
+use Neos\Http\Factories\UriFactory;
 
 /**
  * Boot flow in a behat feature context
@@ -30,6 +31,9 @@ use Neos\Flow\Tests\FunctionalTestRequestHandler;
  */
 trait FlowBootstrapTrait
 {
+    /**
+     * @internal please use {@see self::getObject()} to get objects
+     */
     private static ?Bootstrap $bootstrap = null;
 
     /**
@@ -50,13 +54,22 @@ trait FlowBootstrapTrait
         if (!defined('BEHAT_ERROR_REPORTING')) {
             define('BEHAT_ERROR_REPORTING', E_ALL);
         }
-        self::$bootstrap = new Bootstrap('Testing/Behat');
-        Scripts::initializeClassLoader(self::$bootstrap);
-        Scripts::initializeSignalSlot(self::$bootstrap);
-        Scripts::initializePackageManagement(self::$bootstrap);
-        self::$bootstrap->setActiveRequestHandler(new FunctionalTestRequestHandler(self::$bootstrap));
-        self::$bootstrap->buildRuntimeSequence()->invoke(self::$bootstrap);
-        return self::$bootstrap;
+
+        // Boot flow with an active request handler
+        $flowBootstrap = new Bootstrap('Testing/Behat');
+        $requestHandler = new RuntimeSequenceHttpRequestHandler($flowBootstrap);
+
+        $flowBootstrap->registerRequestHandler($requestHandler);
+        $flowBootstrap->setPreselectedRequestHandlerClassName($requestHandler::class);
+
+        $serverRequestFactory = new ServerRequestFactory(new UriFactory());
+        $request = $serverRequestFactory->createServerRequest('GET', 'http://localhost');
+
+        $requestHandler->setHttpRequest($request);
+
+        $flowBootstrap->run();
+
+        return self::$bootstrap = $flowBootstrap;
     }
 
     /**
