7e51b05690fcfacd587fcb0859eb2316774edda3
Marc Henry Schultz <85400359+mhsdesign@users.noreply.github.com>
2026-04-29 15:03:09 +0200

    TASK: Picknick. Use `str_starts_with` over `preg_match`
    Co-authored-by: Karsten Dambekalns <karsten@dambekalns.de>

--- Classes/FlowBootstrapTrait.php
+++ Classes/FlowBootstrapTrait.php
@@ -56,7 +56,7 @@ trait FlowBootstrapTrait
 
         // Boot flow with an active request handler
         $context = Bootstrap::getEnvironmentConfigurationSetting('FLOW_CONTEXT') ?: 'Testing/Behat';
-        if (preg_match('~^Testing/Behat(/[a-zA-Z]+)?$~', $context) !== 1) {
+        if (!($context === 'Testing/Behat' || str_starts_with($context, 'Testing/Behat/'))) {
             throw new \RuntimeException(sprintf('Invalid behat context %s. Expected to start with Testing/Behat', $context), 1776430103);
         }
         $flowBootstrap = new Bootstrap($context);
